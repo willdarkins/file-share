21db5597e9f2f4ec01a61a856c860601911b3fd6
Will Darkins <willdarkins@gmail.com>
2022-02-25 00:04:35 -0800

    messed with the port call to include full link to local host url

--- server/server.ts
+++ server/server.ts
@@ -3,14 +3,15 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 
 const app = express()
-const port = process.env.PORT
 
 dotenv.config()
 
 //cors middleware to communicate between front,back and db
 app.use(cors());
 
+const port = process.env.PORT
+
 //listen call to port
 app.listen(port , () => {
-    console.log(`🌎 Server is running on PORT ${port}`)
+    console.log(`🌎 Server is running on PORT http://localhost:${port}`)
 })
