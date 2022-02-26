a2a092bbbf129b0249945b8bd290398e3131cb9d
Will Darkins <willdarkins@gmail.com>
2022-02-25 22:54:28 -0800

    uploaded .jpg example with post route

--- server/config/db.ts
+++ server/config/db.ts
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //establishing connection to database
 const connectDB = async () => {
     try {
-        mongoose.connect(process.env.MONGO_URI!, {
+        await mongoose.connect(process.env.MONGO_URI!, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,6 +1,7 @@
-// const File = require('../models/file.model')
 import express from 'express';
 import multer from 'multer';
+import { UploadApiResponse, v2 as cloudinary } from 'cloudinary'
+import File from '../models/file.model'
 
 const router = express.Router();
 
@@ -10,15 +11,36 @@ let upload = multer({
     storage
 })
 
-router.post('/upload', upload.single('myFile'), (req, res) => {
+router.post('/upload', upload.single('myFile'), async (req, res) => {
     try {
         if (!req.file)
             return res.status(400).json({ message: "A file was not provided to upload." })
+        console.log(req.file)
+        let uploadedFile: UploadApiResponse
+        try {
+            uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+                folder: 'Darkins_Folder',
+                resource_type: 'auto'
+            })
+        } catch (error) {
+            console.log(error);
+            return res.status(400).json({ message: "Cloudinary Error." })
+        }
 
-            console.log(req.file)
+        const { originalname } = req.file;
+        const { secure_url, bytes, format } = uploadedFile
+
+        const file = await File.create({
+            filename: originalname,
+            sizeInBytes: bytes,
+            secure_url,
+            format
+        })
+
+        res.status(200).json(file)
     } catch (error) {
         console.log(error);
-            return res.status(500).json({ message: "There has been an error with the server." });
+        return res.status(500).json({ message: "There has been an error with the server." });
     }
 })
 
