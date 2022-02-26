302189755a3c94d84bcf5e6f4f51249ac193ec74
Will Darkins <willdarkins@gmail.com>
2022-02-25 22:18:28 -0800

    confirgured settings for cloudinary api in server.ts

--- server/server.ts
+++ server/server.ts
@@ -3,10 +3,17 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db';
 import router from './routes/index'
+import { v2 as cloudinary } from 'cloudinary'
 
 const app = express()
-
 dotenv.config()
+
+//configure api settings from cloudinary dependency
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_API_CLOUD,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 //function call from db.ts to connect to database
 connectDB();
