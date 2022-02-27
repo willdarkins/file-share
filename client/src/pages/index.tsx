7fca30e4662924e5e77c921bc657ed3d78c87551
Will Darkins <willdarkins@gmail.com>
2022-02-27 14:19:13 -0800

    bare styles finished for dropzone for now... going to make dropzone component next...

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="">
-      <h1>Upload your files and easily share!</h1>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="my-4 text-3x1 font-medium">Upload your files and easily share!</h1>
       <div className="w-96 flex flex-col items-center bg-gray-800 shadow-xl rounded-xl justify-center">
         dropzone
         {/* render file */}
