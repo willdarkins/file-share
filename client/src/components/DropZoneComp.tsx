0f4eb7fa96db365f88e80a254abf6de4f902a84e
Will Darkins <willdarkins@gmail.com>
2022-02-27 14:45:52 -0800

    small style adjustments to dropzone component

--- client/src/components/DropZoneComp.tsx
+++ client/src/components/DropZoneComp.tsx
@@ -8,7 +8,7 @@ const DropZoneComp = () => {
   return (
     <div>
 
-      <div {...getRootProps()}>
+      <div {...getRootProps()} className='h-80 w-full' >
         <input {...getInputProps()} />
         <p>Drag and drop your prefered files</p>
       </div>
