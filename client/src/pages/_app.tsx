af55634b87da9bf02075dd2aaedea1afb6b94550
Will Darkins <willdarkins@gmail.com>
2022-02-27 14:16:52 -0800

    added tailwind sytles to dropzone section of application

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,9 +2,14 @@ import "tailwindcss/tailwind.css";
 import "../../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <div className="grid h-screen font-serif bg-gray-900 text-white place-items-center">
-    <Component {...pageProps} />;
-  </div>
+  return (
+    <div className="grid h-screen font-serif bg-gray-900 text-white place-items-center">
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
+
 }
 
 export default MyApp;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,6 +2,11 @@ export default function Home() {
   return (
     <div className="">
       <h1>Upload your files and easily share!</h1>
+      <div className="w-96 flex flex-col items-center bg-gray-800 shadow-xl rounded-xl justify-center">
+        dropzone
+        {/* render file */}
+        {/* upload button */}
+      </div>
     </div>
   );
 }
