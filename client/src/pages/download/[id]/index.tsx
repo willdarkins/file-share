3a0d406c59d003e7513e020e01dea535bc506d73
Will Darkins <willdarkins@gmail.com>
2022-02-27 22:24:55 -0800

    created function to download files from the download page... imported the js-file-download dependency

--- client/src/pages/download/[id]/index.tsx
+++ client/src/pages/download/[id]/index.tsx
@@ -1,12 +1,21 @@
+import React from 'react'
 import RenderFile from '@components/RenderFile'
 import axios from 'axios'
 import { IFile } from 'libraries/types'
 import { GetServerSidePropsContext, NextPage } from 'next'
-import React from 'react'
+import fileDownload from 'js-file-download'
 
 const index: NextPage<{
     file: IFile
 }> = ({ file: { format, name, sizeInBytes, id } }) => {
+
+const handleDownload = async() => {
+    const { data } = await axios.get(`http://localhost:8000/files/${id}/download`, {
+        responseType: 'blob',
+    });
+    fileDownload(data, name)
+}
+
     return (
         <div className='flex flex-col items-center justify-center py-3 space-y-4 bg-gray-800 rounded-md shadow-xl w-96'>
             {!id ?
@@ -17,7 +26,7 @@ const index: NextPage<{
                 <img src='/images/file-download.png' alt='download' className='w-16 h-16 '/>
                 <h1 className='text-xl '>Your file is ready for download</h1>
                 <RenderFile file={{format,name,sizeInBytes}} />
-                <button className='w-44 bg-gray-900 p-2 my-5 rounded-md focus:outline-none cursor-pointer'>Download</button>
+                <button onClick={handleDownload} className='w-44 bg-gray-900 p-2 my-5 rounded-md focus:outline-none cursor-pointer'>Download</button>
                 </>
         }
         </div>
