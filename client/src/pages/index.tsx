14c2eae1dec6a75fa6cb6908389bbc285c0e26d0
Will Darkins <willdarkins@gmail.com>
2022-02-27 16:19:21 -0800

    had to create new object for file property being passed because it had same syntax as DropZoneComp prop

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import DropZoneComp from "@components/DropZoneComp";
+import RenderFile from "@components/RenderFile";
 
 export default function Home() {
-  const [ file, setFile ] = useState(null)
+  const [file, setFile] = useState(null)
 
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="my-4 text-3x1 font-medium">Upload your files and easily share!</h1>
       <div className="w-96 flex flex-col items-center bg-gray-800 shadow-xl rounded-xl justify-center">
         <DropZoneComp setFile={setFile} />
-        {/* render file */}
+        <RenderFile file={{
+          format: file.type.split('/')[1],
+          name: file.name,
+          sizeInBytes: file.size
+        }} />
         {
           file?.name
         }
