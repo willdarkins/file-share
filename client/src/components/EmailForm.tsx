ef64d69af158dc99117e3c9976c4fb2d432a4ce7
Will Darkins <willdarkins@gmail.com>
2022-02-28 20:39:04 -0800

    created EmailForm component to connect back end email post route to the front end

---
+++ client/src/components/EmailForm.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+
+const EmailForm:FC<{
+id:string
+}> = ({id}) => {
+  return (
+    <div>
+        <h3>Email Your File Directly to Friends!</h3>
+    </div>
+  )
+}
+
+export default EmailForm
